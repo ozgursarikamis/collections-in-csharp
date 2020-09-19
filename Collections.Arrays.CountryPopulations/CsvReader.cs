5172f1b605afe1cb393184820d495b5512cee3bf
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-09-19 07:13:12 +0300

    ReadCountryFromCsvLine method

--- Collections.Arrays.CountryPopulations/CsvReader.cs
+++ Collections.Arrays.CountryPopulations/CsvReader.cs
@@ -11,8 +11,20 @@ namespace Collections.Arrays.CountryPopulations
 
         public Country[] ReadFirstNCountries(int nCountries)
         {
-            return null;
+            Country[] countries = new Country[nCountries];
+            return countries;
         }
 
+        public Country ReadCountryFromCsvLine(string csvLine)
+        {
+            string[] parts = csvLine.Split(',');
+
+            string name = parts[0];
+            string code = parts[1];
+            string region = parts[2];
+
+            int population = int.Parse(parts[3]);
+            return new Country(name, code, region, population);
+        }
     }
 }
