7f6d2226b7635e9e35533fc4c8690ba7a93fd740
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-09-19 03:22:44 +0300

    Arrays project added

---
+++ Collections.Arrays/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Collections.Arrays
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
