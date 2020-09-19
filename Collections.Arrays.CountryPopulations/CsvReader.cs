7160336f674cdc7ce74ff5b641a935ec22c2df0f
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-09-19 03:49:59 +0300

    country project added

---
+++ Collections.Arrays.CountryPopulations/CsvReader.cs
@@ -0,0 +1,18 @@
+namespace Collections.Arrays.CountryPopulations
+{
+    public class CsvReader
+    {
+        private readonly string _csvFilePath;
+
+        public CsvReader(string csvFilePath)
+        {
+            _csvFilePath = csvFilePath;
+        }
+
+        public Country[] ReadFirstNCountries(int nCountries)
+        {
+            return null;
+        }
+
+    }
+}
---
+++ Collections.Arrays.CountryPopulations/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace Collections.Arrays.CountryPopulations
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            FileInfo fileInfo = new FileInfo(
+                Path.Combine(
+                    Environment.CurrentDirectory, "Pop by Largest Final.csv"));
+            
+            CsvReader reader = new CsvReader(fileInfo.Name);
+
+            Country[] countries = reader.ReadFirstNCountries(10);
+
+            foreach (var country in countries)
+            {
+                Console.WriteLine($"{PopulationFormatter.FormatPopulation(country.Population)}");
+            }
+        }
+    }
+}
