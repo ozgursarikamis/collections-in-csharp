6f377b6729b987c0bc9481e849656055d4dfd7aa
Özgür SARIKAMIŞ <ozgursarikamis@gmail.com>
2020-09-19 17:35:30 +0300

    First equality test

---
+++ Advanced/Collections.ArrayEquality/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Collections.ArrayEquality
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime[] bankHols = {
+                new DateTime(2020, 12, 31),
+                new DateTime(2021, 1, 1),
+            };
+
+            DateTime[] bankHols2 = {
+                new DateTime(2020, 12, 31),
+                new DateTime(2021, 1, 1),
+            };
+
+            System.Console.WriteLine($"hols1 == hols2 ?: { bankHols == bankHols2 }");
+        }
+    }
+}
